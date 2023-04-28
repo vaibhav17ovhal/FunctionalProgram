f32670a688307072c9618d32dd9a52dd844003c7
vaibhav17ovhal <125484163+vaibhav17ovhal@users.noreply.github.com>
2023-04-29 00:52:59 +0530

    Update Program.cs

--- FunctionalProgram/Program.cs
+++ FunctionalProgram/Program.cs
@@ -10,42 +10,6 @@ namespace FunctionalProgram
 
             Console.WriteLine("=============X=============");
 
-            //FlipCoin.DisplayPercentage();
-
-            Console.WriteLine("=============X=============");
-
-            //LeapYear.CheckingLeapYear();
-
-            Console.WriteLine("=============X=============");
-
-            //PowerOfTwo.CalculatingPower();
-
-            Console.WriteLine("=============X=============");
-
-            //HarmonicNumber.Harmonic();
-
-            Console.WriteLine("=============X=============");
-
-            //Factors.FactorPrime();
-
-            Console.WriteLine("=============X=============");
-
-            //QuotientAndRemainder.FindQuotientAndRemainder();
-
-            Console.WriteLine("=============X=============");
-
-            //SwapNumbers.SwappingNumbers();
-
-            Console.WriteLine("=============X=============");
-
-            //EvenAndOdd.CheckingEvenOrOdd();
-
-            Console.WriteLine("=============X=============");
-
-            //VowelsAndConsonants.CheckAlphabet();
-
-            Console.WriteLine("=============X=============");
-
             LargestNumber.FindLargestNumber();
         }
     }
